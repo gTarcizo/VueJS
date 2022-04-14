07c330cd3151d5d4ee2c3c6bd135a5bfdf4ae2dc
gTarcizo <90656673+gTarcizo@users.noreply.github.com>
2022-04-14 17:03:59 -0300

    Testes em Vue

---
+++ VueTest/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+<main>
+  <h1>Teste Título</h1>
+  <button type="button" class="btn btn-dark">Dark</button>
+</main>
+
+</template>
+
+<style>
+ @import url(./assets/base.css);
+
+h1{
+  color: blue;
+}
+</style>
